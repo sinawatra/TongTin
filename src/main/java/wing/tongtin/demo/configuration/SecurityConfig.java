c2bd48a9b0be1af4ae706a303542fc0bda0c69eb
JPSE <watrasinarith@gmail.com>
2026-05-01 23:51:31 +0700

    fix: axios errror

--- src/main/java/wing/tongtin/demo/configuration/SecurityConfig.java
+++ src/main/java/wing/tongtin/demo/configuration/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
-                "https://tong-tin-admin.vercel.app"
+                "https://tong-tin-admin.vercel.app",
+                "https://tong-tin-admin.vercel.app/login"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
